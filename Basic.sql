afd4a3de754307597469859b4a8f7f3358b5e5aa
Usama Tahir <usamatahir00004@gmail.com>
2024-11-10 16:13:20 +0100

    Create,Insert,Alter

---
+++ Basic.sql
@@ -0,0 +1,26 @@
+-- Create table 
+CREATE TABLE table_name (
+    column_id INT AUTO_INCREMENT PRIMARY KEY,
+    column_name column_type
+);
+
+
+-- Insert table 
+INSERT INTO table_name (column1, column2)
+VALUES (value1, value2);
+
+-- Alter table (Add a Column)
+ALTER TABLE table_name
+ADD column_name data_type;
+
+-- Alter table (Drop a Column)
+ALTER TABLE table_name
+DROP COLUMN column_name;
+
+-- Alter table (Modify a Columns Data Type)
+ALTER TABLE table_name
+MODIFY column_name new_data_type;
+
+-- Alter table (Rename a column)
+ALTER TABLE table_name
+RENAME TO new_table_name
