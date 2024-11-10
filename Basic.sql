8bd578834247a6b069a4e9693970edb51769458e
Usama Tahir <usamatahir00004@gmail.com>
2024-11-10 16:55:40 +0100

    Modifiying Basic Queries

--- Basic.sql
+++ Basic.sql
@@ -9,6 +9,7 @@ CREATE TABLE table_name (
 INSERT INTO table_name (column1, column2)
 VALUES (value1, value2);
 
+
 -- Alter table (Add a Column)
 ALTER TABLE table_name
 ADD column_name data_type;
@@ -24,3 +25,7 @@ MODIFY column_name new_data_type;
 -- Alter table (Rename a column)
 ALTER TABLE table_name
 RENAME TO new_table_name
+
+-- Alter table (Add a constraint)
+ALTER TABLE table_name 
+ADD CONSTRAINT constraint_name constraint_type (column_name);
