f87d410180727b786b2ff01aea1940a76ad669b7
Usama Tahir <usamatahir00004@gmail.com>
2024-11-27 23:26:57 +0100

    List all employees with their job titles and department names

--- Basic.sql
+++ Basic.sql
@@ -57,3 +57,21 @@ JOIN
     departments d ON e.department_id = d.department_id
 ORDER BY 
     e.employee_id;
+
+--List all employees with their job titles and department names
+
+SELECT 
+    e.employee_id,
+    e.first_name,
+    e.last_name,
+    j.job_title,
+    d.department_name,
+    e.salary
+FROM 
+    employees e
+JOIN 
+    jobs j ON e.job_id = j.job_id
+JOIN 
+    departments d ON e.department_id = d.department_id
+ORDER BY 
+    e.employee_id;
