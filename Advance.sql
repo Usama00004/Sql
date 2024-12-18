4ec706f6ff4261f3a7bae2b7239bbd897c3881e6
Usama Tahir <usamatahir00004@gmail.com>
2024-12-18 16:16:54 +0100

    Adding Querey

--- Advance.sql
+++ Advance.sql
@@ -206,6 +206,18 @@ WHERE e.salary = (
     FROM employees
     WHERE department_id = e.department_id
 );
+----------------------------------------------------------------------------------------------------------------------
+-- Find the highest-paid employee in each department
+SELECT e.employee_id, e.first_name, e.last_name, e.salary, d.department_name
+FROM employees e
+JOIN departments d ON e.department_id = d.department_id
+WHERE e.salary = (
+    SELECT MAX(salary)
+    FROM employees
+    WHERE department_id = e.department_id
+);
+
+
 
 
 
