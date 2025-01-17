c4c81a3c7c72adb03d2ad3b4b7dcf3ad259b8284
Usama Tahir <usamatahir00004@gmail.com>
2025-01-17 09:05:47 +0100

    Procedures

---
+++ Stored_Procedures.sql
@@ -0,0 +1,16 @@
+-- Basic Syntax
+
+CREATE PROCEDURE procedure_name
+AS
+BEGIN
+    -- SQL statements
+END;
+
+
+-- Simple Procedure
+CREATE PROCEDURE GetAllEmployees
+AS
+BEGIN
+    SELECT * FROM Employees;
+END;
+
