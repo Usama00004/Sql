d300f643749ceddf2eda6303fc515a63df01039e
Usama Tahir <usamatahir00004@gmail.com>
2025-01-20 06:26:01 +0100

    Advantages and Disadvantage of Procedures

--- Stored_Procedures.sql
+++ Stored_Procedures.sql
@@ -22,3 +22,17 @@ EXEC GetAllEmployees()
 
 
 
+
+
+
+-------- Advantages of Using a Procedures -------
+-- Precomplied and cashed , reducing parcing time
+-- Eliminate Repetative Code in Querey
+-- Centeralized Logic
+-- Enable Complex operations with BEGIN , TRANSACTION , COMMIT and ROLLBACK
+--
+--
+
+-------- Disadvantages of Using a Procedures -------
+-- Overuse of procedures can leads to tightly coupled database login
+-- Portability to other Data Base Mangement Systems is Difficult
